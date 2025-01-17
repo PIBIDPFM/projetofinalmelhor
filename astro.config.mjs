3305c03fda7ac38e71feb6e6d5b3e75b4009f058
Francisco Marcelaro <velacorvusgames@gmail.com>
2025-01-17 16:28:38 -0300

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://pibidpfm.github.io/projetofinalmelhor",
+  base: "projetofinalmelhor",
+  integrations: [tailwind(), mdx(), sitemap(), icon()],
+});
